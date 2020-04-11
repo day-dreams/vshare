2df983a8d64a29d0764124abc8f53767dfe5f6f7
day-dreams <xyz.zhangnan@gmail.com>
2020-04-11 22:37:36 +0800

    dev: add cors

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/location"
 
 	"github.com/gin-gonic/gin"
@@ -16,6 +17,9 @@ func main() {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.New()
 	r.Use(location.Default())
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+	}))
 
 	r.GET("/", func(c *gin.Context) {
 		c.Data(http.StatusOK, "text/html; charset=utf-8", utils.Index())
@@ -25,7 +29,7 @@ func main() {
 	r.GET("/api/room/status/read", handler.StatusRead())
 	r.GET("/api/room/status/write", handler.StatusWrite())
 	r.GET("/api/play/m3u8/playlist", handler.M3u8PlayList())
-	r.GET("/api/play/m3u8/ts", handler.M3u8PlayList())
+	r.GET("/api/play/m3u8/ts", handler.M3u8TsFile())
 
 	r.Run(os.Getenv("addr")) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
